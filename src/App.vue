88cdbfcb7d1c9caecb6de2ad8d6551364753d64f
Zadkiel <mr.cerrada.zadkiel@gmail.com>
2023-03-01 17:28:40 +0800

    initial scaffolding

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,366 @@
-<script setup lang="ts">
+<script lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import { defineComponent, ref, watch } from 'vue'
+import {
+  MenuOutlined,
+  QqOutlined,
+  SettingOutlined,
+  AppstoreOutlined,
+  DoubleLeftOutlined,
+  SearchOutlined,
+  MailOutlined,
+  FlagOutlined,
+  EditOutlined,
+  DeleteOutlined
+} from '@ant-design/icons-vue'
+
+export default defineComponent({
+  components: {
+    MenuOutlined,
+    QqOutlined,
+    SettingOutlined,
+    AppstoreOutlined,
+    DoubleLeftOutlined,
+    SearchOutlined,
+    FlagOutlined,
+    MailOutlined,
+    EditOutlined,
+    DeleteOutlined
+  },
+  setup() {
+    const selectedKeys = ref(['1'])
+    const openKeys = ref(['sub1'])
+    const handleClick = (e) => {
+      console.log('click', e)
+    }
+    const titleClick = (e) => {
+      console.log('titleClick', e)
+    }
+    watch(
+      () => openKeys,
+      (val) => {
+        console.log('openKeys', val)
+      }
+    )
+    return {
+      selectedKeys,
+      openKeys,
+      handleClick,
+      titleClick,
+      dataSource: [
+        {
+          id: 1,
+          name: 'Princess Abuyo',
+          phone: '09239900912',
+          website: 'dyosa.com',
+          companyName: 'Dyosa Inc.',
+          status: 'Danger'
+        },
+        {
+          id: 2,
+          name: 'Princess Abuyo',
+          phone: '09239900912',
+          website: 'dyosa.com',
+          companyName: 'Dyosa Inc.',
+          status: 'Pending'
+        },
+        {
+          id: 3,
+          name: 'Princess Abuyo',
+          phone: '09239900912',
+          website: 'dyosa.com',
+          companyName: 'Dyosa Inc.',
+          status: 'Info'
+        },
+        {
+          id: 4,
+          name: 'Princess Abuyo',
+          phone: '09239900912',
+          website: 'dyosa.com',
+          companyName: 'Dyosa Inc.',
+          status: 'Danger'
+        },
+        {
+          id: 5,
+          name: 'Princess Abuyo',
+          phone: '09239900912',
+          website: 'dyosa.com',
+          companyName: 'Dyosa Inc.',
+          status: 'Danger'
+        },
+        {
+          id: 69,
+          name: 'Princess Abuyo',
+          phone: '09239900912',
+          website: 'dyosa.com',
+          companyName: 'Dyosa Inc.',
+          status: 'Danger'
+        }
+      ],
+
+      columns: [
+        {
+          title: '#',
+          dataIndex: 'id',
+          key: 'id'
+        },
+        {
+          title: 'CUSTOMER',
+          dataIndex: 'name',
+          key: 'name'
+        },
+        {
+          title: 'PHONE',
+          dataIndex: 'phone',
+          key: 'phone'
+        },
+        {
+          title: 'WEBSITE',
+          dataIndex: 'website',
+          key: 'website'
+        },
+        {
+          title: 'COMPANY NAME',
+          dataIndex: 'companyName',
+          key: 'companyName'
+        },
+        {
+          title: 'STATUS',
+          dataIndex: 'status',
+          key: 'status',
+          slots: { customRender: 'status' }
+        },
+        {
+          title: 'ACTIONS',
+          dataIndex: 'actions',
+          key: 'actions',
+          slots: { customRender: 'actions' }
+        }
+      ]
+    }
+  }
+})
 </script>
 
 <template>
-  <header>
+  <div style="display: flex">
+    <!-- SIDEBAR -->
+    <div>
+      <div style="height: 60px; background: #1b1812; color: white">
+        <div style="display: grid; place-content: center; height: 100%; font-size: 20px">
+          <p style="margin-bottom: 0">Advance Exteriors <DoubleLeftOutlined /></p>
+        </div>
+      </div>
+      <a-menu
+        id="dddddd"
+        v-model:openKeys="openKeys"
+        v-model:selectedKeys="selectedKeys"
+        style="width: 256px; height: 100vh; background: #2b2b2b; color: white"
+        mode="inline"
+        @click="handleClick"
+      >
+        <a-menu-item key="0">
+          <template #icon>
+            <AppstoreOutlined />
+          </template>
+          <span>Dashboard</span>
+        </a-menu-item>
+
+        <p
+          style="
+            margin-left: 20px;
+            margin-bottom: 20px;
+            margin-top: 20px;
+            color: gray;
+            font-weight: bold;
+          "
+        >
+          CUSTOM
+        </p>
+
+        <a-menu-item key="0">
+          <template #icon>
+            <SettingOutlined />
+          </template>
+          <span>Users</span>
+        </a-menu-item>
+
+        <a-menu-item key="0">
+          <template #icon>
+            <SettingOutlined />
+          </template>
+          <span>Albums</span>
+        </a-menu-item>
+
+        <a-menu-item key="0">
+          <template #icon>
+            <SettingOutlined />
+          </template>
+          <span>Photos</span>
+        </a-menu-item>
+
+        <p
+          style="
+            margin-left: 20px;
+            margin-bottom: 20px;
+            margin-top: 20px;
+            color: gray;
+            font-weight: bold;
+          "
+        >
+          FEATURE
+        </p>
+
+        <a-menu-item key="0">
+          <template #icon>
+            <SettingOutlined />
+          </template>
+          <span>Subcontractor</span>
+        </a-menu-item>
+
+        <a-menu-item key="0">
+          <template #icon>
+            <SettingOutlined />
+          </template>
+          <span>Supplier</span>
+        </a-menu-item>
+
+        <a-menu-item key="0">
+          <template #icon>
+            <SettingOutlined />
+          </template>
+          <span>Calendar</span>
+        </a-menu-item>
+
+        <a-menu-item key="0">
+          <template #icon>
+            <SettingOutlined />
+          </template>
+          <span>File Upload</span>
+        </a-menu-item>
+
+        <a-menu-item key="0">
+          <template #icon>
+            <SettingOutlined />
+          </template>
+          <span>Widgets</span>
+        </a-menu-item>
+
+        <a-menu-item key="0">
+          <template #icon>
+            <SettingOutlined />
+          </template>
+          <span>Calendar</span>
+        </a-menu-item>
+
+        <a-menu-item key="0">
+          <template #icon>
+            <SettingOutlined />
+          </template>
+          <span>Charts</span>
+        </a-menu-item>
+
+        <a-menu-item key="0">
+          <template #icon>
+            <SettingOutlined />
+          </template>
+          <span>Maps</span>
+        </a-menu-item>
+      </a-menu>
+    </div>
+
+    <!-- CONTENT -->
+    <div style="width: calc(100% - 256px)">
+      <div
+        style="
+          height: 60px;
+          display: flex;
+          align-items: center;
+          padding: 0 20px;
+          justify-content: space-between;
+        "
+      >
+        <div>
+          <MenuOutlined />
+          <a-button style="margin-left: 1rem; background: #e8e8e8; color: #1890ff; border: none"
+            >Pages</a-button
+          >
+        </div>
+        <div style="display: flex; align-items: center">
+          <SearchOutlined class="header-icons" />
+          <MailOutlined class="header-icons" />
+          <FlagOutlined class="header-icons" />
+          <p>Hi Admin</p>
+          <a-button style="margin-left: 1rem; background: #00bb97; color: white; border: none"
+            >A</a-button
+          >
+        </div>
+      </div>
+      <div
+        style="
+          height: 60px;
+          display: flex;
+          align-items: center;
+          padding: 0 20px;
+          justify-content: space-between;
+        "
+      >
+        <div style="display: flex; align-items: center; justify-content: space-between ; width: 100%">
+          <div style="display: flex; align-items: center">
+            <p>Contacts</p>
+            <a-form style="height: 32px; margin-left: 16px">
+              <a-form-item>
+                <a-input placeholder="Search" style="background: #E8E8E8" /> 
+              </a-form-item>
+            </a-form>
+          </div>
+
+          <a-button style="margin-left: 1rem; background: #e8e8e8; color: #1890ff; border: none"
+            >Add Contacts</a-button
+          >
+        </div>
+        <div style="display: flex; align-items: center"></div>
+      </div>
+      <div style="background: #eeeeee; padding: 20px; height: calc(100vh - 120px)">
+        <a-card :bordered="false">
+          <a-table :dataSource="dataSource" :columns="columns" :pagination="false">
+            <template #status="{ record }">
+              <a-button
+                v-if="record.status === 'Danger'"
+                class="table-status-btn"
+                style="background: #ff3857"
+                >DANGER</a-button
+              >
+              <a-button
+                v-if="record.status === 'Pending'"
+                class="table-status-btn"
+                style="background: #9e64f7"
+                >PENDING</a-button
+              >
+              <a-button
+                v-if="record.status === 'Info'"
+                class="table-status-btn"
+                style="background: #fecc1d"
+                >INFO</a-button
+              >
+            </template>
+            <template #actions="{ record }">
+              <EditOutlined style="margin-right: 16px; cursor: pointer" />
+              <DeleteOutlined style="cursor: pointer" />
+            </template>
+          </a-table>
+
+          <div style="display: flex; justify-content: space-between; margin-top: 16px">
+            <a-button type="primary" :disabled="true" style="border-radius: 8px">Previous</a-button>
+            <a-button type="primary" :disabled="true" style="border-radius: 8px">Next</a-button>
+          </div>
+        </a-card>
+      </div>
+    </div>
+  </div>
+
+  <!-- <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
@@ -16,69 +372,22 @@ import HelloWorld from './components/HelloWorld.vue'
       </nav>
     </div>
   </header>
-  <RouterView />
+  <RouterView /> -->
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+p {
+  margin: 0;
+}
+.header-icons {
+  font-size: 20px;
+  padding-right: 32px;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
+.table-status-btn {
+  color: white;
+  border-radius: 8px;
+  font-weight: bold;
   font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'ant-design-vue/dist/antd.css';
+
+// import Vue3Material from 'vue3-material';
+
+// import VueMaterial from 'vue-material'
+// import 'vue-material/dist/vue-material.min.css'
+// import 'vue-material/dist/theme/default.css'
 
 // import './assets/main.css'
-
+// Vue.use(VueMaterial)
 
 const app = createApp(App)
 
 app.use(router)
+// app.use(Vue3Material)
 
 app.mount('#app')
