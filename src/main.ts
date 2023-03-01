773930bab933432e11526ebeae5a2c9cc9849ee2
Zadkiel <mr.cerrada.zadkiel@gmail.com>
2023-03-01 15:49:38 +0800

    remove material from react

--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,8 @@ import router from './router'
 
 // import './assets/main.css'
 
-import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import { createTheme } from '@mui/material/styles';
-
-export const theme = createTheme({});
 
 const app = createApp(App)
-app.use(ThemeProvider);
-app.use(CssBaseline);
-
-
 
 app.use(router)
 
